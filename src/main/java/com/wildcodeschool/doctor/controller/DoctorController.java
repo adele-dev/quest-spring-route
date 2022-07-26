a053cc370fbfd9b2935c5742e8c880a32f75b404
Adèle <aguessya@yahoo.fr>
2022-07-26 19:06:36 +0200

    ajout routes

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,32 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
-    @ResponseBody
-    public Doctor doctor() {
 
-        return null;
+    @GetMapping("/doctor/{number}")
+    @ResponseBody
+    public Doctor doctor(@PathVariable int number) {
+        if (number == 13) {
+            return new Doctor(number, "Jodie Wittacker");
+        }
+        else if (number >= 1 && number <= 12) {
+
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER, "See other");
+
+        }
+        else {
+            String message = String.format("Impossible de récupérer l'incarnation %s", number);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, message);
+        }
     }
 }
+
